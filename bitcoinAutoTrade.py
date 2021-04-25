f6176c2468bdfc7e82a5abd89f8d2313f72e75e4
PDDia <83182971+PDDia@users.noreply.github.com>
2021-04-26 03:47:27 +0900

    Add files via upload

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -1,9 +1,18 @@
 import time
 import pyupbit
 import datetime
+import requests
 
-access = "your-access"
-secret = "your-secret"
+access = "qlJhQRynlQUVoFI6Dx5ChxDye3vAJabIWYD0zGjj"
+secret = "Yj3UuofnxeexUnXZQihoqFRU51MbItxbCX6jiPxy"
+myToken = "xoxb-2011558263633-2011559894785-tdPjDuYvr8ERmGy4QGInSJuk"
+
+def post_message(token, channel, text):
+    """슬랙 메시지 전송"""
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": text}
+    )
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
@@ -17,11 +26,17 @@ def get_start_time(ticker):
     start_time = df.index[0]
     return start_time
 
-def get_balance(ticker):
+def get_ma15(ticker):
+    """15일 이동 평균선 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=15)
+    ma15 = df['close'].rolling(15).mean().iloc[-1]
+    return ma15
+
+def get_balance(coin):
     """잔고 조회"""
     balances = upbit.get_balances()
     for b in balances:
-        if b['currency'] == ticker:
+        if b['currency'] == coin:
             if b['balance'] is not None:
                 return float(b['balance'])
             else:
@@ -34,26 +49,31 @@ def get_current_price(ticker):
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
+# 시작 메세지 슬랙 전송
+post_message(myToken,"#crypto", "autotrade start")
 
-# 자동매매 시작
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTC")
+        start_time = get_start_time("KRW-DOGE")
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.5)
-            current_price = get_current_price("KRW-BTC")
-            if target_price < current_price:
+            target_price = get_target_price("KRW-DOGE", 0.5)
+            ma15 = get_ma15("KRW-DOGE")
+            current_price = get_current_price("KRW-DOGE")
+            if target_price < current_price and ma15 < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    buy_result = upbit.buy_market_order("KRW-DOGE", krw*0.9995)
+                    post_message(myToken,"#crypto", "DOGE buy : " +str(buy_result))
         else:
-            btc = get_balance("BTC")
+            btc = get_balance("DOGE")
             if btc > 0.00008:
-                upbit.sell_market_order("KRW-BTC", btc*0.9995)
+                sell_result = upbit.sell_market_order("KRW-DOGE", btc*0.9995)
+                post_message(myToken,"#crypto", "DOGE buy : " +str(sell_result))
         time.sleep(1)
     except Exception as e:
         print(e)
+        post_message(myToken,"#crypto", e)
         time.sleep(1)
